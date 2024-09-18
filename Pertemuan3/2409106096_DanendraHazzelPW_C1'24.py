4cd0cb9d3bb8b9d57ac38c65620870d1538f2937
Anzwarith <hazzel0709@gmail.com>
2024-09-18 09:14:57 +0100

    Upload coding pertemuan3

---
+++ Pertemuan3/2409106096_DanendraHazzelPW_C1'24.py
@@ -0,0 +1,45 @@
+# cuaca = "cerah"
+# if cuaca == "cerah" :
+#     print("mari jalan-jalan")
+# else :
+#     print("diam di rumah saja")
+
+
+# umur = int(input("masukkan umur : "))
+# if umur > 0:
+#     if umur <= 10:
+#         print("anda masih anak-anak")
+#     elif umur <= 18:
+#         print("anda masih remaja")
+#     elif umur <= 35:
+#         print("anda sudah dewasa") 
+#     elif umur <= 65:
+#         print("anda sudah paruh baya")
+#     else :
+#         print ("anda sudah tua")
+# else :
+#     print("input usia harus bilangan positif")        
+
+
+
+# nim = int(input("masukkan nim  :  "))
+
+# if nim >= 1 and nim <= 90 : 
+#     if nim >= 1 and nim <= 45 :
+#         print("Kelas A")
+#     else :
+#         print("Kelas B")
+# elif nim >= 91 and nim <= 120 :
+#     if nim >= 91 and nim <= 120 :
+#         print("Kelas C")
+# else :
+#     print("Anda bukan siswa Informatika 24")
+
+
+# angka = int(input("Masukkan angka : "))
+# hasil = "Genap" if angka % 2 == 0 else "Ganjil"
+# print (angka, 'adalah ',hasil)
+       
+
+# nim = int(input("masukkan nim : "))
+# hasil = "Kelas A" if nim >= 1 and nim <=50 else "Kelas B" if nim >= 51 and nim 100 else "Kelas C" if nim >= 101 <= 120
